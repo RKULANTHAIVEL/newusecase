e87989569074c713937a3516ba57597bc1f0d3e7
KULANTHAIVELANBUSELVAN.R <kulanthaivelanbuselvan@gmail.com>
2026-04-21 11:03:03 +0530

    Add TicTacToe class with user input functionality

--- newuc3.java
+++ newuc3.java
@@ -14,14 +14,14 @@ class TicTacToe {
                 slot = sc.nextInt();
 
                 if (slot >= 1 && slot <= 9) {
-                    return slot; // valid input
+                    return slot; 
                 } else {
                     System.out.println("Invalid input! Enter number between 1 and 9.");
                 }
 
             } else {
                 System.out.println("Invalid input! Please enter a number.");
-                sc.next(); // clear invalid input
+                sc.next(); 
             }
         }
     }
